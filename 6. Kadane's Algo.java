79ae8f5da4d1f0f09568811f37c8bb4c8786893b
atulThynk <atul.dahiya@thynkweb.com>
2025-01-24 22:02:28 +0530

    kadane's

--- 6. Kadane's Algo.java
+++ 6. Kadane's Algo.java
@@ -1,5 +1,27 @@
 //Brute force approach
 
+// class Solution {
+
+//     // arr: input array
+//     // Function to find the sum of contiguous subarray with maximum sum.
+//     int maxSubarraySum(int[] arr) {
+
+        
+//         int maxSum=Integer.MIN_VALUE;
+        
+//         for(int i=0;i<arr.length;i++){
+//             int currSum=0;
+//             for(int j=i;j<arr.length;j++){
+//                 currSum+=arr[j];
+//                 maxSum=Math.max(maxSum,currSum);
+//             }
+//         }
+//         return maxSum;
+//     }
+// }
+
+//Kadane's algorithm
+
 class Solution {
 
     // arr: input array
@@ -9,15 +31,14 @@ class Solution {
         
         int maxSum=Integer.MIN_VALUE;
         
+        int currSum=0;
         for(int i=0;i<arr.length;i++){
-            int currSum=0;
-            for(int j=i;j<arr.length;j++){
-                currSum+=arr[j];
-                maxSum=Math.max(maxSum,currSum);
+            currSum+=arr[i];
+            maxSum=Math.max(currSum,maxSum);
+            if(currSum<0){
+                currSum=0;
             }
         }
         return maxSum;
     }
 }
-
-//Kadane's algorithm
