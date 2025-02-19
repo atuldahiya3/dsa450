3011f2450793c9587717b6949457b220e1a44d2a
atulThynk <atul.dahiya@thynkweb.com>
2025-02-19 22:55:18 +0530

    question

--- 1. Array/16.SubArrayBeauty.java
+++ 1. Array/16.SubArrayBeauty.java
@@ -1,5 +1,7 @@
-class Solution {
-    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+    public int[] getSubarrayBeautyBruteForce(int[] nums, int k, int x) {
         ArrayList<Integer> res=new ArrayList<>();
         int l=nums.length;
         int temp=0;
@@ -21,4 +23,40 @@ class Solution {
             return 0;
         }
     }
+
+    //optimized
+    
+        public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+            int l=nums.length;
+            //-> initialize array counter to store count record of negative elements from -50 to -1
+            int[] counter=new int[50]; 
+            //-> ans array to store xth smallest element of each subarray
+            int[] ans=new int[l-k+1];
+    
+            for(int i=0;i<l;i++){
+                //-> if the element is negative, increment its count in the counter array in positive terms by adding 50
+                if(nums[i]<0){
+                    counter[nums[i]+50]++;
+                }
+                //-> if the element is negative but outside the subarray limit, decrement its count in the counter array in positive terms by adding 50
+                if(i-k>=0 && nums[i-k]<0){
+                    counter[nums[i-k]+50]--;
+                }
+                //-> keep sliding untill subarray of size k is reached
+                if(i-k+1<0) continue;
+                int count=0;
+                //-> iterate over the counter array to find the xth smallest element
+                for(int j=0;j<50;j++){
+                    //-> increment count with the elements conatining elements in counter array, only orignal negative value elements are there in counter array
+                    count+=counter[j];
+                    //-> if xth smallest element is found, store it in ans array and break the loop
+                    if(count>=x){
+                        ans[i-k+1]=j-50;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    
 }
