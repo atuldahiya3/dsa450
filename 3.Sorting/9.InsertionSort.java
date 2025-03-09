0191ccce68a484f6af49395e5ab2cd326d2446e3
atuldahiya3 <atulsinghdahiya@gmail.com>
2025-03-09 15:47:44 +0530

    sort

--- 3.Sorting/9.InsertionSort.java
+++ 3.Sorting/9.InsertionSort.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        int curr=0;
+        int prev=0;
+        for(int i=1;i<arr.length;i++){
+            curr=arr[i];
+            prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+    }
+}
