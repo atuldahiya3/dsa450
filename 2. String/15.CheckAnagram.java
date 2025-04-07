d1cdf0118510ee9a317260f05c3f05fde2da0791
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2025-04-08 00:43:21 +0530

    string anagram

---
+++ 2. String/15.CheckAnagram.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution15 {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer> data= new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            data.put(s.charAt(i),data.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            data.put(t.charAt(i),data.getOrDefault(t.charAt(i),0)-1);
+        }
+        for(int val:data.values()){
+            if(val!=0) return false;
+        }
+        return true;
+    }
+}
