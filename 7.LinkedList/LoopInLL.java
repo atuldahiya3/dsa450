a808abf634272b0b5d2ea68aee81314bd2b14646
atuldahiya3 <atulsinghdahiya@gmail.com>
2025-04-20 22:14:28 +0530

    LL

--- 7.LinkedList/LoopInLL.java
+++ 7.LinkedList/LoopInLL.java
@@ -1,4 +1,4 @@
-public class Solution14 {
+public class LoopInLL {
     public ListNode detectCycle(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
@@ -16,4 +16,12 @@ public class Solution14 {
         }
         return head;
     }
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 }
