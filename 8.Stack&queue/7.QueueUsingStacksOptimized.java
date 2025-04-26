ad2810b421a9bcaaa594274dd77e350ae3f9b94a
atuldahiya3 <atulsinghdahiya@gmail.com>
2025-04-27 00:37:14 +0530

    queue and stack

--- 8.Stack&queue/7.QueueUsingStacksOptimized.java
+++ 8.Stack&queue/7.QueueUsingStacksOptimized.java
@@ -1,4 +1,4 @@
-class MyQueue6 {
+class MyQueue {
     Stack<Integer> s1;
     Stack<Integer> s2;
     public MyQueue() {
@@ -7,17 +7,23 @@ class MyQueue6 {
     }
     
     public void push(int x) {
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
         s1.push(x);
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
     }
     
     public int pop() {
-        shift();
-        return s2.pop();
+        // shift();
+        return s1.pop();
     }
     
     public int peek() {
-        shift();
-        return s2.peek();
+        // shift();
+        return s1.peek();
     }
     
     public boolean empty() {
@@ -31,14 +37,3 @@ class MyQueue6 {
         }
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
-
- 
