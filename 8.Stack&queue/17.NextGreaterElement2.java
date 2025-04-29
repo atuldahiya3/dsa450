c8fcdf20c17c4f379d91e49677a57936b3ffe53c
atuldahiya3 <atulsinghdahiya@gmail.com>
2025-04-29 23:48:05 +0530

    question

--- 8.Stack&queue/17.NextGreaterElement2.java
+++ 8.Stack&queue/17.NextGreaterElement2.java
@@ -1,3 +1,21 @@
-public class 17.NextGreaterElement2 {
-    
+import java.util.Stack;
+
+class Solution17 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int arr[]= new int[n];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            arr[i]=-1;
+        }
+        for(int i=0;i<n*2;i++){
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i%n]){
+                arr[stack.pop()]=nums[i%n];
+            }
+            if(i<n){
+                stack.push(i);
+            }
+        }
+        return arr;
+    }
 }
