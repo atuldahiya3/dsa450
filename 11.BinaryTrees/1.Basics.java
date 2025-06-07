b2225324b716a2a7e819e09d1283733b97b2cc00
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2025-06-07 23:53:03 +0530

    trees

--- 11.BinaryTrees/1.Basics.java
+++ 11.BinaryTrees/1.Basics.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class basics{
     static class Node{
         int data;
@@ -22,7 +25,7 @@ class basics{
             return newNode;
         }
     }
-    public static void preorder(Node root){
+    public static void preorder(Node root){  
         if(root==null){
             // System.out.print("-1 ");
             return;
@@ -31,11 +34,62 @@ class basics{
         preorder(root.left);
         preorder(root.right);
     }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        inorder(root.right);
+        System.out.print(root.data+" ");
+    }
+    public static void levelOrder(Node root){
+        Queue<Node> q= new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode=q.poll();
+            if(currNode==null){
+                System.out.println("");
+                if(!q.isEmpty()){
+                    q.add(null);
+                }
+            }else{
+                System.out.print(currNode.data+" ");
+                if(currNode.left!=null) q.add(currNode.left);
+                if(currNode.right!=null) q.add(currNode.right);
+            }
+        }
+    }
+    public static int countNodes(Node root){
+        if(root==null) return 0;
+        int leftNode=countNodes(root.left);
+        int rightNode=countNodes(root.right);
+        return leftNode+rightNode+1;
+    }
+    public static int sumNodes(Node root){
+        if(root==null) return 0;
+        int leftNode=countNodes(root.left);
+        int rightNode=countNodes(root.right);
+        return leftNode+rightNode+1;
+    }
     public static void main(String[] args){
         int[] data={1, 2, 4, -1,-1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree=new BinaryTree();
         Node root= tree.buildTree(data);
         System.out.println("Binary Tree created with root value: " + root.data);
-        preorder(root);
+        // preorder(root);
+        // inorder(root);
+        // postorder(root);
+        levelOrder(root);
+        System.out.println("Total number of nodes in the tree: " + countNodes(root));
     }
+    
 }
