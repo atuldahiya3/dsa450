40d95290f03e8f0835f2c8971d052ef036740788
atulThynk <87331579+atuldahiya3@users.noreply.github.com>
2025-08-13 23:45:24 +0530

    optimized

--- 1. Array/79.2SumSorted.java
+++ 1. Array/79.2SumSorted.java
@@ -2,6 +2,26 @@ class Solution {
     public ArrayList<Integer> twoSum(int[] arr, int target) {
         // code here
         ArrayList<Integer> res= new ArrayList<>();
+        int n=arr.length;
+        int i=0,j=n-1;
+        while(i<j){
+            if(arr[i]+arr[j]==target){
+                res.add(i+1);
+                res.add(j+1);
+                return res;
+            }else if(arr[i]+arr[j]>target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        res.add(-1);
+        res.add(-1);
+        return res;
+    }
+    public ArrayList<Integer> twoSum2(int[] arr, int target) {
+        // code here
+        ArrayList<Integer> res= new ArrayList<>();
         HashMap<Integer,Integer> map= new HashMap<>();
         for(int i=0;i<arr.length;i++){
             if(map.containsKey(target-arr[i])){
